91332072bf73029e9216b017c3e51ade62750c50
leewt95 <leewt.15@gmail.com>
2018-04-03 15:42:08 +0900

    Updated Pyramid.h with functions.

--- Pyramid.h
+++ Pyramid.h
@@ -1,3 +1,9 @@
+#ifndef PYRAMID_H
+#define PYRAMID_H
+
+#include <cmath.h>
+using namespace std;
+
 class Pyramid{
 public:
 	double length;
@@ -5,5 +11,25 @@ public:
 	double height;
 	
 private:
+	
+	double getBaseArea() {
+		return length * width;	
+	}
+	
+	double getTriAreaLengthHeight() {
+		return length * (sqrt(pow(width/2, 2) + pow(height, 2));
+	}
+				 
+	double getTriAreaWidthHeight() {
+		return width * (sqrt(pow(length/2, 2) + pow(height, 2));
+	}	
 
+	double getVolumePyramid() {
+		return (getBaseArea() * height)/3;
+	}
+	
+	double getSurfaceArea() {
+		return getBaseArea() + getTriAreaLengthHeight() + getTriAreaWidthHeight();
+	}
 };
+#endif
