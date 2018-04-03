5f3cd6f1b3d8354f9c86a286f2fe04590adb6b48
chanmy <shaoran.cmy@hotmail.com>
2018-04-03 13:57:44 +0800

    update pyramid: area(4), volume

--- Pyramid.h
+++ Pyramid.h
@@ -1,3 +1,8 @@
+#ifndef PYRAMID_H
+#define PYRAMID_H
+
+#include <math.h
+
 class Pyramid{
 public:
 	double length;
@@ -5,5 +10,24 @@ public:
 	double height;
 	
 private:
-
+	double getBaseArea(){
+		return width * length;
+	}
+	
+	double getLHTriArea(){
+		return 0.5 * length * sqrt(pow(height, 2) + pow(width/2, 2));
+	}
+	
+	double getWHTriArea(){
+		return 0.5 * width * sqrt(pow(height, 2) + pow(length/2, 2));
+	}
+	
+	double totalArea(){
+		return getBaseArea + 2 * getLHTriArea() + 2 * getWHTriArea();
+	}
+	
+	double getVolume(){
+		return legnth * width * height / 3;
+	}
 };
+#endif
