87cc24072899fb07af49adbe8d88046c4e95f5ce
chanmy <shaoran.cmy@hotmail.com>
2018-04-03 13:33:47 +0800

    Update Sphere: getArea, getVolume

--- Sphere.h
+++ Sphere.h
@@ -1,7 +1,19 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include <math.h>
+
 class Sphere{
 public:
 	double radius;
 	
 private:
-
+	double getArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2);	
+	}
+	
+	double getVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3);	
+	}
 };
+#endif
