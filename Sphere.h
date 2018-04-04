d516e3f30dbfd7f1a48ff085325c338e57d78637
chanmy <shaoran.cmy@hotmail.com>
2018-04-04 22:19:03 +0800

    update Sphere.h: constructor, multiple function

--- Sphere.h
+++ Sphere.h
@@ -4,16 +4,46 @@
 #include <math.h>
 
 class Sphere{
-public:
+private:
 	double radius;
 	
-private:
-	double getArea(){
+public:
+	Sphere(){}
+	
+	Sphere(double r){
+		radius = r;	
+	}
+	
+	double getFullArea(){
 		return 4.0 * 22 / 7 * pow(radius, 2);	
 	}
 	
-	double getVolume(){
+	double halfArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 2;	
+	}
+	
+	double quarterArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 4;	
+	}
+	
+	double eighthArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 8;	
+	}
+	
+	double getFullVolume(){
 		return 4.0 / 3 * 22 / 7 * pow(radius, 3);	
+	}
+	
+	double halfVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 2;	
+	}
+	
+	double quarterVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 4;	
+	}
+	
+	double eighthVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 8;	
 	}
 };
 #endif
