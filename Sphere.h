e1ec339477622a6016ecf00836b0386fc0c6a239
Meng Yan <shaoran.cmy@hotmail.com>
2018-04-05 15:33:22 +0800

    b

--- Sphere.h
+++ Sphere.h
@@ -4,5 +4,48 @@
 #include <math.h>
 
 class Sphere{
+private:
+	double radius;
+	
+public:
+	Sphere(double r){
+		radius = r;	
+	}
+	
+	double setRadius(double r) {
+		radius = r;
+	}
+	
+	double getFullArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2);	
+	}
+	
+	double halfArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 2;	
+	}
+	
+	double quarterArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 4;	
+	}
+	
+	double eighthArea(){
+		return 4.0 * 22 / 7 * pow(radius, 2) / 8;	
+	}
+	
+	double getFullVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3);	
+	}
+	
+	double halfVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 2;	
+	}
+	
+	double quarterVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 4;	
+	}
+	
+	double eighthVolume(){
+		return 4.0 / 3 * 22 / 7 * pow(radius, 3) / 8;
+	}
 };
 #endif
