803b02984acd62fdec157d08febc9d65325600bb
Meng Yan <shaoran.cmy@hotmail.com>
2018-04-05 15:32:26 +0800

    b

--- Sphere.h
+++ Sphere.h
@@ -4,6 +4,23 @@
 #include <math.h>
 
 class Sphere{
-
+public:
+	Sphere(double r){
+		radius = r;	
+	}
+	
+private:
+	double setRadius(double r) {
+		radius = r;
+	}
+	double getSurfaceArea() {
+		return 4 * 3.142 * pow(radius, 2);
+	}
+	double getDiameterRadius() {
+		return 2 * radius;	
+	}
+	double getVolumeSphere() {
+		return (4/3) * 3.142 * pow(radius, 3);	
+	}
 };
 #endif
